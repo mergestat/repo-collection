bbb64bba3cc6289188035223ca2f46b49902b958
Patrick DeVivo <patrick.devivo@gmail.com>
2023-05-06 10:50:36 -0400

    feat: updated the script - limit the repos to collect

--- main.ts
+++ main.ts
@@ -5,6 +5,8 @@ import { sleep } from "https://deno.land/x/sleep/mod.ts";
 const octokit = new Octokit({ auth: Deno.env.get("GITHUB_TOKEN") });
 const db = new DB("repos.db");
 
+const MAX_REPOS_PER_LANGUAGE = 500;
+
 Deno.addSignalListener("SIGINT", () => {
   insertRepo.finalize();
   db.close();
@@ -28,51 +30,26 @@ const languages = [
   "Java",
   "C++",
   "C#",
-  "Visual Basic",
   "JavaScript",
-  "SQL",
   "PHP",
   "Go",
-  "Assembly",
-  "R",
-  "MATLAB",
-  "R",
   "Swift",
   "Ruby",
   "Rust",
-  "Fortran",
-  "SAS",
-  "Ada",
   "Objective-C",
   "Perl",
-  "F#",
-  "Dart",
   "Lisp",
-  "Lua",
-  "Julia",
   "Scala",
-  "PLSQL",
   "Haskell",
   "TypeScript",
-  "D",
   "Kotlin",
-  "PowerShell",
-  "Shell",
-  "Groovy",
   "Zig",
-  "Scheme",
-  "Prolog",
   "Elixir",
-  "HCL",
   "Erlang",
-  "Clojure",
-  "COBOL",
-  "VBA",
-  "YAML",
-  "CoffeeScript",
 ]
 
 for await (const language of languages) {
+  let repoCount = 0
   const iterator = octokit.paginate.iterator('GET /search/repositories', {
     headers: {
       'X-GitHub-Api-Version': '2022-11-28'
@@ -85,6 +62,11 @@ for await (const language of languages) {
   
   for await (const { data: repos } of iterator) {
     for (const repo of repos) {
+      repoCount++
+      if (repoCount > MAX_REPOS_PER_LANGUAGE) {
+        break
+      }
+      
       insertRepo.execute([repo.id, JSON.stringify(repo)])
       console.log(`inserted repo ${repo.id} (${repo.owner.login}/${repo.name})`)
     }
